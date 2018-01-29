2ebc595208026cb4f26f564aff29b789baa99ae1
Ken Rayner <kenneth.rayner@digital.hmrc.gov.uk>
2018-01-29 17:16:24 +0000

    add numbers and reverse functions

---
+++ reverse.scala
@@ -0,0 +1,15 @@
+package app
+
+import scala.io.StdIn
+
+object ReversedSeq extends App {
+
+  def makeList(Number: Int): List[Int] = {
+    val x = (1 to Number).toList.reverse
+    x
+  }
+
+  println("Please enter a number:")
+  var userVal = StdIn.readInt
+  println(makeList(userVal))
+}
